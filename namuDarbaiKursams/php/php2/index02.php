9ffbf1b86a7e38d999e4bfe7b4ec3734a8e9d2fb
hyneryl <hyneryl@gmail.com>
2019-01-31 09:00:44 +0200

    adding php2 folder updates

---
+++ namuDarbaiKursams/php/php2/index02.php
@@ -0,0 +1,21 @@
+<?php
+	$country = 'Lietuva'; 
+	$city = 'Vilnius'; 
+	$population = 574221;
+?>
+
+
+<!--exercice 2.1-->
+<p><?php echo $city . ' - Lietuvos sostinė' ; ?></p>
+
+<!--exercice 2.2-->
+
+<p><?php echo " $city ". "ir " . $country . " sparčiai auga." ; ?></p>
+
+<!--exercice 2.3-->
+  
+<?php 
+	$largest_population = "true";
+	echo $largest_population; 
+?>
+
