fb81bb06ff69c541c7373d4ac66bd159f78cf1fd
hyneryl <hyneryl@gmail.com>
2019-01-31 08:53:25 +0200

    adding php folders 2nd try

---
+++ namuDarbaiKursams/php/php1/index.php
@@ -0,0 +1,11 @@
+<?php
+	$country = 'Lietuva'; 
+	$city = 'Vilnius'; 
+	$population = 574221;
+?>
+
+<ul>
+	<li><?php echo $country  ; ?></li>
+	<li><?php echo $city  ; ?></li>
+	<li><?php echo $population  ; ?></li>
+</ul>
