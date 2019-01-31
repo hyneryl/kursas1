4a0e6891ad6e8981f789408c8ec52fe796d251a8
hyneryl <hyneryl@gmail.com>
2019-01-31 09:15:53 +0200

    adding php3 folder updates

---
+++ namuDarbaiKursams/php/php3/index03.php
@@ -0,0 +1,20 @@
+<!--exercice 3-->
+<br>
+<?php 
+	$x = 10; 
+	$y = 7;
+	$z = $x + $y;
+	echo "$x + $y = $z <br />"; 
+	
+	$z = $x - $y;
+	echo "$x - $y = $z <br />";
+
+	$z = $x * $y;
+	echo "$x * $y = $z <br />";
+
+	$z = $x / $y;
+	echo "$x / $y = $z <br />";
+
+	$z = $x % $y;
+	echo "$x % $y = $z <br />";
+?> 
